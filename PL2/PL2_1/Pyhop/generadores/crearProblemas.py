c74ce4ac82363e0e82f33f80ded20bdfe02c3577
nacho123121 <nachopm0013@gmail.com>
2023-05-11 19:58:28 +0100

    final

--- PL2/PL2_1/Pyhop/generadores/crearProblemas.py
+++
@@ -1,7 +0,0 @@
-import os
-
-#tamaño = [40, 50, 60, 70, 80, 90, 100, 125, 150, 175]
-tamaño = [46, 47, 48]
-
-for i in tamaño:
-    os.system("python3 ./generate-problem2_1.py -d 1 -l "+str(i)+" -p "+str(i)+" -c "+str(i)+" -g "+str(i))
